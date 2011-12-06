40ee3d736a746dd2705863ead167707d337cdf7e
Erik Berman <contact@nukleo.fr>
2011-12-06 17:14:11 +0100

    V1.0 almost ready to go!

--- jquery.nukleopageflip.js
+++ jquery.nukleopageflip.js
@@ -21,9 +21,9 @@
 			easing:				"easeInOutExpo",	// easing. if easing plugin is being used, if not change to swing or linear
 			shadowOn:			true,				// show the centerfold shadow ?
 			shadowOpacity:		0.5,				// opacity of the centerfold shadow
-			resizeOnLoad:		false,				// downsize on load -> usefull for small screens (think smartphone...)
 			vMargin:			10,					// minimum vertical margin from window (used for scaling)
 			hMargin:			80,					// minimum horizontal margin from window (used for scaling)
+			showNav:			true,				// show previous and next buttons ?
 			navOpacityStart:	0.5,				// previous and next opacity
 			navOpacityEnd:		1,					// previous and next hover opacity
 			directNav:			true				// direct page navigation below the pageflip
@@ -40,9 +40,9 @@
 		var $images = $('img', $pages);		// cache les images (peut etre pas utile)
 		var countPages = $pages.length;		// number of pages
 		var currentPage = 0;				// current page
-		var pageWidth = $images.eq(0).width();	// largeur d'une page (calculé en fonction des images)
-		var pageHeight = $images.eq(0).height();	// hauteur d'une page (calculé en fonction des images)
-		var urlParam = '';					// parametre d'url pour aller directement à une page
+		var pageWidth;						// largeur d'une page (calculé en fonction des images)
+		var pageHeight;						// hauteur d'une page (calculé en fonction des images)
+		var urlParam;						// parametre d'url pour aller directement à une page
 		// ------------------------------------------------------------------------------------------------------------------
 
 
@@ -56,8 +56,6 @@
 		var prevHeight = $buttonPrev.height();
 		var nextHeight = $buttonNext.height();
 		$wrapper.append($buttonPrev, $buttonNext);
-
-		// TODO: cache button sizes
 
 		// Build, insert and hide centerfold shadow
 		var $shadow = $('<div id="page-shadow">').css({'height':pageHeight, 'left':pageWidth-50, 'opacity':opts.shadowOpacity}).hide();
@@ -98,14 +96,12 @@
 			// User clicked the previous button
 			if($(this).is('.previous')) {
 				var prev = getNextPreviousPage('previous'); // get previous page number
-				//console.log('curPage: '+currentPage+' | PrevPage: '+prev);
 				flipPrev(prev);
 			}
 
 			// User clicked the next button
 			else if($(this).is('.next')) {
 				var next = getNextPreviousPage('next'); // get next page number
-				//console.log('curPage: '+currentPage+' | NexPage: '+next);
 				flipNext(next);
 			}
 			return false; // don't let the browser's default click action occur
@@ -119,12 +115,10 @@
 			if(currentPage == targetPage) {
 				return false;
 			}
-			//console.log('current:'+currentPage+' | Target:'+targetPage)
 			jumpToPage(targetPage);
 			return false;
 		});
 
-		// TODO : rescale on window resize
 
 
 		// FUNCTIONS ---------------------------------------------------------------------------------------------------------
@@ -157,7 +151,6 @@
 				showNavButtons(page);
 				currentPage = page;
 				updatePagination(page);
-				//console.log('new curPage: '+currentPage);
 			});
 
 			// affichage de la nouvelle page de droite si ce n'est pas la dernière
@@ -186,7 +179,6 @@
 				showNavButtons(page);
 				currentPage = page;
 				updatePagination(page);
-				//console.log('new curPage: '+currentPage);
 			});
 
 			// anim page de gauche
@@ -209,11 +201,9 @@
 			$pages.css({'z-index':0}); // reset z-index on all pages
 
 			if(page > currentPage) {
-				//console.log('going to next: '+page);
 				flipNext(page);
 			}
 			else {
-				//console.log('going to prev: '+page);
 				flipPrev(page);
 			}
 		};
@@ -221,10 +211,12 @@
 
 		// affichage des bouton prev/next
 		function showNavButtons(page){
-			$buttonPrev.css({'top':(pageHeight/2)-(prevHeight/2)});
-			$buttonNext.css({'top':(pageHeight/2)-(nextHeight/2)});
-			if(page != 0) {$buttonPrev.fadeIn();}
-			if(page != countPages) {$buttonNext.fadeIn();}
+			if(opts.showNav){
+				$buttonPrev.css({'top':(pageHeight/2)-(prevHeight/2)});
+				$buttonNext.css({'top':(pageHeight/2)-(nextHeight/2)});
+				if(page != 0) {$buttonPrev.fadeIn();}
+				if(page != countPages) {$buttonNext.fadeIn();}
+			}
 		};
 
 
@@ -268,48 +260,17 @@
 		};
 
 
-		// Downscaling to fit smaller screens. Only works on pageload for now.
-		// Maybe implement on resize and upscaling
-		function scalePage(){
-			var winHeight = $(window).height()-opts.vMargin;
-			var winWidth = $(window).width()-opts.hMargin;
-			// get real height of all elements
-			var realHeight = $wrapper.outerHeight(true) + $('#pageflip-header').outerHeight(true);
-			//var margin = realHeight - $wrapper.outerHeight();
-			var scale;
-
-			// pages plus hautes que le viewport
-			if(realHeight > winHeight){
-				scale = (winHeight) / (realHeight);
-				console.log("1 : "+scale);
-			}
-
-			if(pageWidth*2 > winWidth){
-				scale = (winWidth / pageWidth) / 2;
-				console.log("2 : "+scale);
-			}
-
-			if(realHeight > winHeight || pageWidth*2 > winWidth){
-				pageHeight = pageHeight*scale;
-				pageWidth = pageWidth*scale;
-				$images.width(pageWidth);
-				$images.height(pageHeight);
-				console.log("final : "+scale);
-			}
-		};
-
 		// initialise the plugin
 		function initPageflip(){
+			// init size variables, based on the pageflip's first image
+			pageWidth = $images.eq(0).width();	// largeur d'une page (calculé en fonction des images)
+			pageHeight = $images.eq(0).height();
+			$pages.hide().css({opacity:1});
 			$pageflip.css({'height': pageHeight, 'overflow':'hidden'});
 			$wrapper.css({'height': pageHeight, 'width':pageWidth*2});
 
 			// no direct page access in the url OR 1st page OR out of bounds -> go to first page
-			if( urlParam == '' || ( urlParam == 1 || (urlParam < 1 && urlParam > countPages) ) ) {
-				if(opts.resizeOnLoad) {
-					scalePage();
-					$wrapper.css({'width':pageWidth*2, 'height':pageHeight});
-					$pageflip.css({'width':pageWidth*2, 'height':pageHeight});
-				}
+			if( urlParam == undefined || ( urlParam == 1 || (urlParam < 1 && urlParam > countPages) ) ) {
 				$pages.eq(0).css('left', pageWidth).fadeIn(opts.speed);
 				showNavButtons(0);
 				updatePagination(0);
@@ -319,11 +280,6 @@
 			else {
 				urlParam = parseInt(urlParam);
 				urlParam = (urlParam % 2 != 0) ? urlParam-1 : urlParam;
-				if(opts.resizeOnLoad) {
-					scalePage();
-					$wrapper.css({'width':pageWidth*2, 'height':pageHeight});
-					$pageflip.css({'width':pageWidth*2, 'height':pageHeight});
-				}
 				jumpToPage(urlParam);
 				updatePagination(urlParam);
 			}
@@ -332,16 +288,20 @@
 
 		// init the init...
 		function init(){
-			$pages.hide();
+			$pages.css({opacity:0});
 			if(!$images[0].complete){
-				setTimeOut(init(), 200);
+				setTimeout(init, 500);
 			}
 			else {
+				$(loader).fadeOut();
 				initPageflip();
 			}
 		};
 
 		// ------------------------------------------------------------------------------------------------------------------
+
+		// overlay indicating the first image load
+		var loader = $('<div id="loader">').text('Chargement en cours...').prependTo($pageflip);
 
 		// go!!!
 		init();
